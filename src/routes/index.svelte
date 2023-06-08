244663c754e3f12d9e86d0f8809d1d2b65fc85a2
Peter Stadler <14peterstadler@gmail.com>
2023-06-09 00:01:09 +0200

    add first Thailand

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -376,8 +376,10 @@ electronics:</p>
 
        <Card headline="Cambodia" text="test text" />
             <Card headline="Vietnam" text="test text" />
-                 <Card headline="Thailand" text="test text" />
+                 <Card headline="Thailand 2" text="test text" />
                       <Card headline="Laos" text="test text" />
+                           <Card headline="Thailand 1" text="test text" />
+
 
 
 
